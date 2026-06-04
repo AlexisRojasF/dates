7c86b83d05611900c0525598d07c8ad04e3f680c
elijsdev <elizabethpazp695@gmail.com>
2026-06-04 14:36:18 -0600

    feat: add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: '¿Saldrías conmigo? 🐱',
@@ -9,7 +10,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
-      <body>{children}</body>
+      <body>{children}<Analytics /></body>
     </html>
   );
 }
